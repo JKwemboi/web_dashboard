031c02dc772615e13068da714f8395f424be2d73
Joshua Kwemboi <joshuakwemboi03@gmail.com>
2026-04-08 12:07:49 +0300

    dashboard changes

--- detection/views.py
+++ detection/views.py
@@ -1,14 +1,24 @@
-from django.http import StreamingHttpResponse
-from .camera import get_frames
-from django.shortcuts import render
-from .models import Detection
-
-
-def video_feed(request):
-    return StreamingHttpResponse(get_frames(),
-                                 content_type='multipart/x-mixed-replace; boundary=frame')
+from django.shortcuts import render, redirect
+from django.contrib.auth.models import User, auth
+from django.contrib import messages
 
 
 def dashboard(request):
-    detections = Detection.objects.all().order_by('-timestamp')[:10]
-    return render(request, 'dashboard.html', {'detections': detections})
+    return render(request, 'login.html')
+
+
+def login(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+
+        user = auth.authenticate(username=username, password=password)
+
+        if user is not None:
+            auth.login(request, user)
+            return redirect('/')
+        else:
+            messages.error(request, 'Invalid Credentials')
+            return redirect('login')
+    else:
+        return render(request, 'login.html')
