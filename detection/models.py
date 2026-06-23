a3953a991330f796520aef3acfabe2c7977fb58d
Joshua Kwemboi <joshuakwemboi03@gmail.com>
2026-06-23 14:22:54 +0300

    update templates & models

--- detection/models.py
+++ detection/models.py
@@ -1,15 +1,5 @@
 from django.db import models
 
-
-# class Detection (models.Model):
-#     is_lion = models.BooleanField(default=False)
-#     location = models.CharField(default="Unknown", max_length=100)
-#     confidence = models.FloatField(null=True, blank=True)
-#     label = models.CharField(max_length=255)
-#     timestamp = models.DateTimeField(auto_now_add=True)
-#     image = models.ImageField(upload_to='detections/images', null=True)
-
-
 class Alerts(models.Model):
     alert_type = models.CharField(max_length=255)
     message = models.TextField()
--- detection/views.py
+++ detection/views.py
@@ -1,4 +1,3 @@
-import cv2
 from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth.models import User, auth
 from django.contrib import messages
